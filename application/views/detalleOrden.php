3c7a9ded31cb89745ffa1e2de1c68861b362d212
JaimeGonz <valdoc7@gmail.com>
2021-01-26 10:58:53 -0600

    Agrega diseño a ordenes realizadas

--- application/views/detalleOrden.php
+++ application/views/detalleOrden.php
@@ -1,10 +1,11 @@
 <script src="<?=base_url()?>assets/detalleOrden.js"></script>
+<link rel="stylesheet" href="<?=base_url()?>assets/css/empleados.css">
 
 <!-- ************************************************************************* -->
 <div class="content-wrapper">
 	<div class="content">
 		<div class="card">
-		  <div class="card-header">
+		  <div class="card-header"  style="background: #fff; margin: 15px 0;">
 		  	<div class="" style="text-align: center;" >
 		  		<h2>Detalle orden de trabajo</h2>
 		  	</div>
@@ -12,74 +13,79 @@
 
 		  <div class="card-body">
 		  	<div>
-		  		<h3>Datos generales</h3><br>
+		  		<p style="color: #000; font-weight: 500">Datos generales</p><br>
 		  		<h4>Cliente</h4><br>
-		  		<table class="table">
-		  			<thead>
-		  				<tr>
-		  					<th>Nombre completo</th>
-		  					<th>Teléfono</th>
-		  					<th>Celular</th>
-		  					<th>Dirección</th>
-		  				</tr>
-		  			</thead>
-		  			<tbody>
-		  				<td><?=$data->cliente?> <?=$data->apellido1?> <?=$data->apellido2?></td>
-		  				<td><?=$data->telefono?></td>
-		  				<td><?=$data->celular?></td>
-		  				<td><b>Calle</b> <?=$data->calle?> <b>#</b> <?=$data->numero?> <b>Col.</b> <?=$data->colonia?> <b>Mpio.</b> <?=$data->municipio?> <b>C.P.</b> <?=$data->cp?></td>
-		  			</tbody>
-		  		</table>
+          <div class="table-responsive" style="display: block;">
+            <table class="table table-hover tabla ">
+              <thead>
+                <tr>
+                  <th class="titles-thead" scope="col">Nombre completo</th>
+                  <th class="titles-thead" scope="col">Teléfono</th>
+                  <th class="titles-thead" scope="col">Celular</th>
+                  <th class="titles-thead" scope="col">Dirección</th>
+                </tr>
+              </thead>
+              <tbody>
+                <td><?=$data->cliente?> <?=$data->apellido1?> <?=$data->apellido2?></td>
+                <td><?=$data->telefono?></td>
+                <td><?=$data->celular?></td>
+                <td style="text-align: center;  ">Calle <?=$data->calle?> # <?=$data->numero?> Col. <?=$data->colonia?>, Mpio. <?=$data->municipio?>, C.P. <?=$data->cp?></td>
+              </tbody>
+            </table>
+          </div>
 
 		  		<h4>Mascota</h4><br>
 
-		  		<table class="table">
-		  			<thead>
-		  				<tr>
-		  					<th>Especie</th>
-		  					<th>Nombre</th>
-		  					<th>Raza</th>
-		  					<th>Pelaje</th>
-		  					<th>Tamaño</th>
-		  					<th>Peso</th>
-		  					<th>Estatura</th>
-		  					<th>Fecha nacimiento</th>
-		  					<th>Edad</th>
-		  				</tr>
-		  			</thead>
-		  			<tbody>
-		  				<td><?=$data->especie?></td>
-		  				<td><?=$data->nombre?></td>
-		  				<td><?=$data->raza?></td>
-		  				<td><?=$data->pelaje?></td>
-		  				<td><?=$data->tamano?></td>
-		  				<td><?=$data->peso?> kg</td>
-		  				<td><?=$data->estatura?> mts</td>
-		  				<td><?=$data->fecha_nacimiento?></td>
-		  				<td> <?php date_default_timezone_set("America/Mexico_City");
-		  							$dia_actual = date("Y-m-d");
-									$edad_diff = date_diff(date_create($data->fecha_nacimiento), date_create($dia_actual));
-									echo $edad_diff->format('%y año(s) %m mes(es) %d dia(s) ');?></td>
-		  			</tbody>
-		  		</table>
-		  		
+          <div class="table-responsive" style="display: block;">
+            <table class="table table-hover tabla">
+              <thead>
+                <tr>
+                  <th class="titles-thead" scope="col">Nombre</th>
+                  <th class="titles-thead" scope="col">Especie</th>
+                  <th class="titles-thead" scope="col">Raza</th>
+                  <th class="titles-thead" scope="col">Pelaje</th>
+                  <th class="titles-thead" scope="col">Tamaño</th>
+                  <th class="titles-thead" scope="col">Peso</th>
+                  <th class="titles-thead" scope="col">Estatura</th>
+                  <th style="text-align: center;" class="titles-thead" scope="col">Fecha nacimiento</th>
+                  <th class="titles-thead" scope="col">Edad</th>
+                </tr>
+              </thead>
+              <tbody>
+                <td><?=$data->nombre?></td>
+                <td><?=$data->especie?></td>
+                <td><?=$data->raza?></td>
+                <td><?=$data->pelaje?></td>
+                <td><?=$data->tamano?></td>
+                <td><?=$data->peso?> kg</td>
+                <td><?=$data->estatura?> m</td>
+                <td style="text-align: center;"><?=$data->fecha_nacimiento?></td>
+                <td style="text-align: center;"> <?php date_default_timezone_set("America/Mexico_City");
+                      $dia_actual = date("Y-m-d");
+                    $edad_diff = date_diff(date_create($data->fecha_nacimiento), date_create($dia_actual));
+                    echo $edad_diff->format('%y año(s) %m mes(es) %d dia(s) ');?></td>
+              </tbody>
+            </table>
+          </div>
 		  	</div>
 
-		  	<div>
-		  		<h3>Servicios</h3>
-		  		<section id="resultado">
-		  			
+		  	<div style="margin-bottom: 20px;">
+		  		<h4 style="margin-bottom: 20px;"3>Servicios</h4>
+		  		<section id="resultado" style="display: flex;">
 		  		</section>
 		  	</div>
-		  	
+        <div class="card-footer" style="background: #fff; margin-top: 15px;">
+          <div class="row" style="margin-top: 15px;">
+            <div class="col-lg-4"></div>
+            <div class="col-lg-8">
+            <a href="<?=base_url()?>welcome/ordenesRealizadas" type="button" class="btn btn-primary">Regresar</a>
+            </div>
+          </div>
+        </div>
 		  	<div>
-		  		<a href="<?=base_url()?>welcome/ordenesRealizadas" type="button" class="btn btn-primary">Regresar</a>
-		  		
-		  	</div>
-   
-		    
 		  </div>
-		</div> 
+		  </div>
+		</div>
 	</div>
 </div>
 <!-- ************************************************************************* -->
--- application/views/ordenesRealizadas.php
+++ application/views/ordenesRealizadas.php
@@ -1,29 +1,26 @@
 <script src="<?=base_url()?>assets/ordenesRealizadas.js"></script>
+<link rel="stylesheet" href="<?=base_url()?>assets/css/empleados.css">
 
 <!-- ************************************************************************* -->
-<div class="content-wrapper">
+<div class="content-wrapper wrapper">
 	<div class="content">
-		<div class="card">
-		  <div class="card-header">
+		<div class="card card-container" style="overflow-y: auto;">
+		  <div class="card-header card-search">
 		  	<div class="" style="text-align: center;" >
-		  		<h2>Ordenes de trabajo realizadas</h2>
+		  		<h2 style="margin-bottom: 30px;" >Órdenes de trabajo realizadas</h2>
 		  	</div>
-          <div class="row">
-            <div class="col-sm-12">
-             <section><input type="text" name="busqueda" id="busqueda" placeholder="Buscar orden de trabajo por especie, raza, nombre..." class="form-control mb-6"></section>
-            </div>
-            
-          </div>
-		  </div>
-
-		  <div class="card-body">
-		    
-            <section id="resultado">
+        <div class="row container-search">
+          <div class="search">
+            <section class="search-input">
+              <input type="text" name="busqueda" id="busqueda" placeholder="Buscar orden de trabajo por especie, raza, nombre..." class="form-control mb-6">
             </section>
-   
-		    
+          </div>
+        </div>
 		  </div>
-		</div> 
+		  <div class="card-body">
+        <section id="resultado"></section>
+		  </div>
+		</div>
 	</div>
 </div>
 <!-- ************************************************************************* -->
